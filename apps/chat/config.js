27c8709b459f023ecb36574fbf4801bace58f467
ernesto <ernestorg@poli.ufrj.br>
2023-07-02 23:48:09 -0300

    no major changes

--- apps/chat/config.js
+++ apps/chat/config.js
@@ -1,6 +1,5 @@
 module.exports = {
     MONGODB:
-    'mongodb+srv://admin:nvY2liWbYuDOsCO1@cluster0.mu5pw.mongodb.net/GRAPHQL?retryWrites=true&w=majority',
     SECRET_KEY: 'some very secret key'
 };
 
