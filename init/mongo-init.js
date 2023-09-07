11e4c6468df250ff20a74fc9e7dff39358a87b22
ernesto <ernestorg@poli.ufrj.br>
2023-09-07 13:55:23 -0300

    add graphql queries

--- init/mongo-init.js
+++ init/mongo-init.js
@@ -9,7 +9,7 @@ db.users.insert(
     { 
         email: "ernesto@mail.com",
         username: "ernesto",
-        password: "$2a$12$fkMZLs2SvglzYmE3wDKJr.6XPeK5qcRrCxNdDLERMkYstxlMceEz2",
+        password: "$2a$12$qywJRHtD3bfxWzZdYz8Wde.kGg3bXv3VxRt2XjgRVFqdDeMtTzPvy",
         createdAt: "2022-01-23T22:36:05.745Z",
         latitude: -22.9144193,
         longitude: -43.2193136,
@@ -17,13 +17,26 @@ db.users.insert(
 
     }
 )
-
 db = db.getSiblingDB("yovend-products")
 db.createUser( 
     {
         user: "root",
         pwd: "example",
         roles: [ "readWrite", "dbAdmin" ]        
+    }
+)
+db.users.insert( 
+    {
+        _id: ObjectId('64f9fd6bd4044d4418914297'),
+        savedProducts: [],
+        email: 'lucia@mail.com',
+        username: 'lucia',
+        password: '$2a$12$qywJRHtD3bfxWzZdYz8Wde.kGg3bXv3VxRt2XjgRVFqdDeMtTzPvy',
+        createdAt: '2023-09-07T16:42:19.456Z',
+        latitude: -22.9628636,
+        longitude: -43.1756065,
+        image: '',
+        __v: 0
     }
 )
 db.products.insert( 
